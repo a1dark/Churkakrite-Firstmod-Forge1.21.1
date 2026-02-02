b9a5e7654bd7e24ca08e05156a4ae142973a0fc4
Aidar Kubanychbekov <aidarstudent24@gmail.com>
2026-02-02 23:12:02 +0100

    Added 2 items and 2 blocks

---
+++ src/main/java/com/aidar/tutorialmod/TutorMod.java
@@ -0,0 +1,81 @@
+package com.aidar.tutorialmod;
+
+import com.aidar.tutorialmod.block.ModBlocks;
+import com.aidar.tutorialmod.item.ModItems;
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(TutorMod.Mod_Id)
+
+public class TutorMod
+{
+    // Define mod id in a common place for everything to reference
+    public static final String Mod_Id = "firstmod";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+    // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
+    public TutorMod(FMLJavaModLoadingContext context)
+    {
+        IEventBus modEventBus = context.getModEventBus();
+
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+
+        // Register the item to a creative tab
+        modEventBus.addListener(this::addCreative);
+
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
+
+        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
+        context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+    }
+
+    // Add the example block item to the building blocks tab
+    private void addCreative(BuildCreativeModeTabContentsEvent event)
+    {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.CHURKAKRITE);
+            event.accept(ModItems.CHURKAKRITE_RAW);
+        }
+        if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS){
+            event.accept(ModBlocks.CHURKAKRITE_BLOCK);
+            event.accept(ModBlocks.RAW_CHURKAKRITE_BLOCK);
+        }
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = Mod_Id, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+        }
+    }
+}
---
+++ src/main/java/com/aidar/tutorialmod/item/ModItems.java
@@ -0,0 +1,19 @@
+package com.aidar.tutorialmod.item;
+
+import com.aidar.tutorialmod.TutorMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.EventBus;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS=
+            DeferredRegister.create(ForgeRegistries.ITEMS, TutorMod.Mod_Id);
+    public static final RegistryObject<Item> CHURKAKRITE= ITEMS.register("churkakrite", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> CHURKAKRITE_RAW=ITEMS.register("churkakrite_raw", () -> new Item(new Item.Properties()));
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
